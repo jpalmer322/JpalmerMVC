cd182cc62342743c8e0887744bcb6f52949812cd
jpalmer322 <jpalmer@icstars.org>
2019-09-14 20:15:42 -0500

    Add project files.

---
+++ MVC/Controllers/HelloWorldController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MVC.Controllers
+{
+    public class HelloWorldController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Welcome(string name = "James", int NumTimes = 4)
+        {
+            ViewData["Message"] = "Hello " + name;
+            ViewData["NumTimes"] = NumTimes;
+            return View();
+        }
+    }
+}
